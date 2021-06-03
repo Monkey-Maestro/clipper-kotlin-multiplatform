273eed6f9a8f3129bb753feb93b4e0fcaee3c957
MonkeyMaestro <muehlhaus@urbanistic.de>
2021-06-03 13:59:26 +0200

    added github action maven deploy to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,44 @@
-import java.lang.System.getenv
+import java.util.*
+
+
+//We need to differentiate if we are building local or if Github Actions are used to build the Project!
+var user_name : String
+var user_pat : String
+var maven_url : String
+var current_spacecenter_tag : String = "1.0.0"
+if(System.getenv("GITHUB_REPOSITORY") != null){
+    //if we are in github we use the environmental variables provided by github
+    user_name = System.getenv("GITHUB_ACTOR")
+    user_pat = System.getenv("GITHUB_TOKEN")
+    maven_url = "https://maven.pkg.github.com/${System.getenv("GITHUB_TOKEN")}"
+    current_spacecenter_tag = System.getenv("GITHUB_TAG")
+}else{
+    //else we need to use our cutsom credentials stored in the local.properties file
+    val local = Properties()
+    local.load(rootProject.file("local.properties").inputStream())
+    user_name = local.getProperty("github.user") //
+    user_pat = local.getProperty("github.token") // pat with scope: read packages
+    maven_url = local.getProperty("maven.url")
+}
+
 
 plugins {
     id("maven-publish")
-    kotlin("multiplatform") version "1.5.0"
+    kotlin("multiplatform") version "1.5.10"
 }
+
 
 allprojects {
     group = "de.urbanistic"
-    version = System.getenv("GITHUB_REF")?.split('/')?.last() ?: "development"
-    //version = "1.0.0"
+    version = current_spacecenter_tag
+    //version = "1.0.1"
 }
+
 
 repositories {
     mavenCentral()
 }
+
 
 kotlin {
     jvm {
@@ -69,17 +94,15 @@ kotlin {
     }
 }
 
-//publish to github maven repository using github actions
-getenv("GITHUB_REPOSITORY")?.let {
-    publishing {
-        repositories {
-            maven {
-                name = "github"
-                url = uri("https://maven.pkg.github.com/$it")
-                credentials(PasswordCredentials::class){
-                    username = System.getenv("GITHUB_ACTOR")
-                    password = System.getenv("GITHUB_TOKEN")
-                }
+
+publishing {
+    repositories {
+        maven {
+            name = "github"
+            url = uri(maven_url)
+            credentials(PasswordCredentials::class) {
+                username = user_name
+                password = user_pat
             }
         }
     }
